38313891415d9640e923e94b766ecaf1eeda37f6
wan-nyan-wan <distributed.system.love@gmail.com>
2021-05-06 15:05:06 +0000

    add tools

--- zsh/tools/cargo.sh
+++ zsh/tools/cargo.sh
@@ -10,19 +10,23 @@ export PATH="$HOME/cargo/.bin:$PATH"
 export RUST_SRC_PATH=$(rustc --print sysroot)/lib/rustlib/src/rust/src/
 
 ## cargo tools
-if [ ! -f ~/.cargo/bin/exa ]; then
-    cargo install exa;
-else
-    alias ls=exa;
-fi
-if [ ! -f ~/.cargo/bin/bat ]; then
-    cargo install --locked bat;
-else
-    alias cat=bat;
-fi
-if [ ! -f ~/.cargo/bin/rg ]; then
-    cargo install ripgrep;
-else
-    alias grep=rg;
-fi
+installCargoPackage() {
+    if [ ! -f ~/.cargo/bin/$2 ]; then
+        cargo install $1;
+    else
+        alias $3=$2;
+    fi
+}
 
+installCargoPackage exa exa ls
+installCargoPackage bat bat cat
+installCargoPackage ripgrep rg grep
+installCargoPackage kmon kmon kmon
+installCargoPackage git-delta delta git-delta
+installCargoPackage procs procs ps
+installCargoPackage du-dust dust dust
+installCargoPackage tokei tokei tokei
+installCargoPackage hyperfine hyperfine hyperfine
+installCargoPackage ytop ytop top
+installCargoPackage bandwhich bandwhich bandwhich
+installCargoPackage grex grex grex
