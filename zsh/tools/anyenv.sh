90e243f28b43a32ef82022119be8de118142cfe5
wan-nyan-wan <distributed.system.love@gmail.com>
2021-05-28 13:00:17 +0900

    fix pyenv problem

--- zsh/tools/anyenv.sh
+++ zsh/tools/anyenv.sh
@@ -1,9 +1,19 @@
 ## anyenv
+
 if [ ! -d ~/.anyenv ]; then
     git clone https://github.com/anyenv/anyenv ~/.anyenv
     yes | ~/.anyenv/bin/anyenv install --init
 fi
 export PATH="$HOME/.anyenv/bin:$PATH"
+
+# TODO remove these snipet if the problem resolve
+export PYENV_ROOT="$HOME/.anyenv/envs/pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
+
 eval "$(anyenv init -)"
 
 source ~/.dotfiles/zsh/tools/anyenv/pyenv.sh
