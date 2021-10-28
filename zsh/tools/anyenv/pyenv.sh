dbe8ed50fe60f66e950f4d95595453922e8228c7
wan-nyan-wan <distributed.system.love@gmail.com>
2021-10-28 17:09:48 +0900

    Fix pyenv warnings

--- zsh/tools/anyenv/pyenv.sh
+++ zsh/tools/anyenv/pyenv.sh
@@ -1,5 +1,16 @@
 ### pyenv
+export PYENV_ROOT="$HOME/.anyenv/envs/pyenv"
+export PYENV_SHELL=zsh
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+# Load completions
+source "$PYENV_ROOT/completions/pyenv.zsh"
+
 if [ ! -d ~/.anyenv/envs/pyenv ]; then
     ~/.anyenv/bin/anyenv install pyenv
 fi
-eval "$(pyenv init -)"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init --path)"
+else
+  eval "$(pyenv init -)"
+fi
