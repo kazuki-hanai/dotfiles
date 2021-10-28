ff779374adafbd5ff5c94fbdd6ade1d60fb30e74
wan-nyan-wan <distributed.system.love@gmail.com>
2021-10-28 08:01:03 +0000

    Remove init.sh

--- zsh/init.sh
+++
@@ -1,9 +0,0 @@
-source ~/.dotfiles/zsh/pkg.sh
-source ~/.dotfiles/zsh/symboliclink.sh
-
-source ~/.dotfiles/zsh/tmux.sh
-source ~/.dotfiles/zsh/zplug.sh
-source ~/.dotfiles/zsh/fzf/fzf.sh
-source ~/.dotfiles/zsh/cargo.sh
-source ~/.dotfiles/zsh/anyenv/anyenv.sh
-source ~/.dotfiles/zsh/nvim.sh
