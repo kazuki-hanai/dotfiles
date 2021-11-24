5da6764dc26f877addefb299544502a40c355945
wan-nyan-wan <distributed.system.love@gmail.com>
2021-11-24 19:12:48 +0900

    Fix small

--- zsh/zsh-configs.sh
+++ zsh/zsh-configs.sh
@@ -14,7 +14,6 @@ zstyle ':completion:*:default' menu select=1
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle :compinstall filename $HOME'/.zshrc'
-bindkey "^[[Z" reverse-menu-complete
 
 autoload -U promptinit; promptinit
 
