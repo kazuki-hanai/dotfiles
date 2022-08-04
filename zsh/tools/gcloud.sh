3aa46c03a7df520184fb5f90149dfb3971951179
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2022-08-04 16:51:59 +0900

    Add gcloud settings

---
+++ zsh/tools/gcloud.sh
@@ -0,0 +1,11 @@
+
+if [ -d ~/google-cloud-sdk ]; then
+  # The next line updates PATH for the Google Cloud SDK.
+  if [ -f ~/google-cloud-sdk/path.zsh.inc ]; then . ~/google-cloud-sdk/path.zsh.inc; fi
+
+  # The next line enables shell command completion for gcloud.
+  if [ -f ~/google-cloud-sdk/completion.zsh.inc ]; then . ~/google-cloud-sdk/completion.zsh.inc; fi
+else
+  echo 'You should install gcloud via https://cloud.google.com/sdk/docs/install'
+  exit 1
+fi
