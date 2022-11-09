8d8f5437ff23d3506ff6036e8bf430bc094098c8
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2022-11-09 17:58:24 +0900

    Add currbr command that show current branch name

--- zsh/symbolic-link.sh
+++ zsh/symbolic-link.sh
@@ -5,9 +5,21 @@ link() {
 }
 
 # symbolic link
+# .zshrc
 link ~/.dotfiles/zsh/.zshrc ~/.zshrc
+
+# alacritty.yml
 mkdir -p ~/.config/alacritty && \
     link ~/.dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+# .gitconfig
 link ~/.dotfiles/.gitconfig ~/.gitconfig
+
+# nvim
 mkdir -p ~/.config && link ~/.dotfiles/nvim ~/.config/nvim
+
+# starship
 mkdir -p ~/.config && link ~/.dotfiles/starship.toml ~/.config/starship.toml
+
+# .bin
+link ~/.dotfiles/.bin ~/.bin
