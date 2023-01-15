87481f7319331ec0cb60a99fe5ee7b9c22bd7410
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-01-15 14:01:54 +0900

    Install tokei and fix installCargoPackage to commonize

--- zsh/tools/cargo.sh
+++ zsh/tools/cargo.sh
@@ -11,20 +11,32 @@ export RUST_SRC_PATH=$(rustc --print sysroot)/lib/rustlib/src/rust/src/
 
 ## cargo tools
 installCargoPackage() {
-    if [ ! -f ~/.cargo/bin/$2 ]; then
-        cargo install $1;
-    fi
-    alias $3=$2;
+  if [ "$#" = 2 ]; then
+    binary_file=$2
+  else
+    binary_file=$1
+  fi
+
+  if [ ! -f ~/.cargo/bin/$binary_file ]; then
+    cargo install $1;
+  fi
 }
 
-installCargoPackage exa exa ls
-installCargoPackage bat bat cat
-installCargoPackage ripgrep rg grep
-installCargoPackage kmon kmon kmon
-installCargoPackage git-delta delta git-delta
-installCargoPackage du-dust dust dust
-installCargoPackage tokei tokei tokei
-installCargoPackage hyperfine hyperfine hyperfine
-installCargoPackage ytop ytop top
-installCargoPackage bandwhich bandwhich bandwhich
-installCargoPackage grex grex grex
+# Install package
+installCargoPackage exa
+installCargoPackage bat
+installCargoPackage ripgrep rg
+installCargoPackage kmon
+installCargoPackage git-delta delta
+installCargoPackage du-dust dust
+installCargoPackage tokei
+installCargoPackage hyperfine
+installCargoPackage ytop
+installCargoPackage bandwhich
+installCargoPackage grex
+installCargoPackage tokei 
+
+# Alias for cargo package
+alias cat=bat
+alias ls=exa
+alias git-delta=delta
