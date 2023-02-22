feaa6871635d4b6356abe7bef211204387552b90
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-02-23 02:10:04 +0900

    update

--- zsh/dotfiles-private.sh
+++ zsh/dotfiles-private.sh
@@ -1,5 +1,10 @@
 # Import dotfiles-private
 if [ ! -d ~/.dotfiles-private ]; then
+  ssh -T git@github.com
+  echo $?
+  if [ "$?" = 1 ]; then
     git clone git@github.com:wan-nyan-wan/dotfiles-private.git ~/.dotfiles-private
+  fi
+else
+  source ~/.dotfiles-private/init.sh
 fi
-source ~/.dotfiles-private/init.sh
