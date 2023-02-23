fa4246dc62512b359ef5bdb58d314cf7ea7065f8
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-02-24 00:58:24 +0900

    update

--- zsh/tools/deno.sh
+++ zsh/tools/deno.sh
@@ -1,3 +1,4 @@
 if [ ! -x "`which deno`" ]; then
-  curl -fsSL https://deno.land/x/install/install.sh | sh
+  # curl -fsSL https://deno.land/x/install/install.sh | sh
+  cargo install deno --locked
 fi
