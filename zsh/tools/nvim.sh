3d376f13bf0b3d143ae9a7c804ade4b6773a6f33
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-02-23 21:11:15 +0900

    update

--- zsh/tools/nvim.sh
+++ zsh/tools/nvim.sh
@@ -3,7 +3,7 @@ local osversion=$(uname -s)
 if ! which nvim 1>/dev/null; then
   if [ "${osversion:0:5}" = 'Linux' ]; then
     sudo apt-add-repository -y ppa:neovim-ppa/stable
-    sudo apt update -y
+    sudo apt update
     sudo apt install -y neovim
   elif [ "${osversion:0:6}" = 'Darwin' ]; then
     brew install neovim
