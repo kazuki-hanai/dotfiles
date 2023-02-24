66952ec42b1052246d3945616c1a170aa6f7f2ff
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-02-25 03:12:37 +0900

    Add debug mode

--- zsh/zsh-configs.sh
+++ zsh/zsh-configs.sh
@@ -1,19 +1,20 @@
-# Zshell configs
+# zsh configs
+
+# completions
+fpath=(/usr/local/share/zsh-completions $fpath)
+fpath+=~/.zfunc
 
 # bashcompinit
-autoload bashcompinit
-bashcompinit
-
-# compinit
+autoload -Uz bashcompinit && bashcompinit
 autoload -Uz compinit && compinit
 
 # zstyle
 setopt auto_list
 setopt auto_menu
-zstyle ':completion:*:default' menu select=1 
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+zstyle ':completion:*:default' menu select=1 
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-zstyle :compinstall filename $HOME'/.zshrc'
+zstyle ':compinstall' filename $HOME'/.zshrc'
 
 autoload -U promptinit; promptinit
 
@@ -22,18 +23,14 @@ bindkey "^[[Z" reverse-menu-complete
 bindkey ^Y backward-word
 bindkey ^O forward-word
 
-# completions
-fpath=(/usr/local/share/zsh-completions $fpath)
-fpath+=~/.zfunc
-
 # esc delay
 KEYTIMEOUT=1
+
 # user systemd
 export XDG_RUNTIME_DIR=/run/user/$UID
 
 # separate char
 export WORDCHARS='*?_[]~=&;!#$%^(){}<>'
-
 
 # default editor
 EDITOR=vim
