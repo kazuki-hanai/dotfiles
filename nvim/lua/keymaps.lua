a42e8f139625618c96c3bfad4025cfc55dee25c3
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-04-04 01:45:02 +0900

    Replace keymaps.vim to keymaps.lua

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,14 @@
+
+-- " common keymap
+vim.api.nvim_set_keymap("i", "<S-Tab>", "<C-d>",                { noremap = true, silent = false })
+
+-- " Tab mappings
+vim.api.nvim_set_keymap("n", "<C-t>",   ":tabnew<CR>",          { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<C-l>",   ":tabnext<CR>",         { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<C-h>",   ":tabprev<CR>",         { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "tl",      ":+tabmove<CR>",        { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "th",      ":-tabmove<CR>",        { noremap = true, silent = false })
+vim.api.nvim_set_keymap("i", "<C-t>",   "<Esc>:tabnew<CR>i",    { noremap = true, silent = false })
+vim.api.nvim_set_keymap("i", "<C-l>",   "<Esc>:tabnext<CR>i",   { noremap = true, silent = false })
+vim.api.nvim_set_keymap("i", "<C-h>",   "<Esc>:tabprev<CR>i",   { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<C-h>",   "<Esc>:tabprev<CR>i",   { noremap = true, silent = false })
