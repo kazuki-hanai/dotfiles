6f2c6d4dc01b3c11739f40eeaa3b0b4deabf5020
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-04-05 02:33:43 +0900

    refactor keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,14 +1,32 @@
+local map = vim.api.nvim_set_keymap
 
--- " common keymap
-vim.api.nvim_set_keymap("i", "<S-Tab>", "<C-d>",                { noremap = true, silent = false })
+-- Common keymap
+map("i", "<S-Tab>", "<C-d>", { noremap = true, silent = false })
 
--- " Tab mappings
-vim.api.nvim_set_keymap("n", "<C-t>",   ":tabnew<CR>",          { noremap = true, silent = false })
-vim.api.nvim_set_keymap("n", "<C-l>",   ":tabnext<CR>",         { noremap = true, silent = false })
-vim.api.nvim_set_keymap("n", "<C-h>",   ":tabprev<CR>",         { noremap = true, silent = false })
-vim.api.nvim_set_keymap("n", "tl",      ":+tabmove<CR>",        { noremap = true, silent = false })
-vim.api.nvim_set_keymap("n", "th",      ":-tabmove<CR>",        { noremap = true, silent = false })
-vim.api.nvim_set_keymap("i", "<C-t>",   "<Esc>:tabnew<CR>",     { noremap = true, silent = false })
-vim.api.nvim_set_keymap("i", "<C-l>",   "<Esc>:tabnext<CR>",    { noremap = true, silent = false })
-vim.api.nvim_set_keymap("i", "<C-h>",   "<Esc>:tabprev<CR>",    { noremap = true, silent = false })
-vim.api.nvim_set_keymap("n", "<C-h>",   "<Esc>:tabprev<CR>",    { noremap = true, silent = false })
+-- Tab mappings
+map("n", "<C-t>", ":tabnew<CR>",        { noremap = true, silent = false })
+map("n", "<C-l>", ":tabnext<CR>",       { noremap = true, silent = false })
+map("n", "<C-h>", ":tabprev<CR>",       { noremap = true, silent = false })
+map("n", "tl",    ":+tabmove<CR>",      { noremap = true, silent = false })
+map("n", "th",    ":-tabmove<CR>",      { noremap = true, silent = false })
+map("i", "<C-t>", "<Esc>:tabnew<CR>",   { noremap = true, silent = false })
+map("i", "<C-l>", "<Esc>:tabnext<CR>",  { noremap = true, silent = false })
+map("i", "<C-h>", "<Esc>:tabprev<CR>",  { noremap = true, silent = false })
+map("n", "<C-h>", "<Esc>:tabprev<CR>",  { noremap = true, silent = false })
+
+-- NerdTree
+map("n", "<leader>n", ":NERDTreeFind<cr>",   { noremap = true, silent = false })
+map("n", "<cs-n>",    ":NERDTreeToggle<cr>", { noremap = true, silent = false })
+
+-- Telescope
+map("n", "<leader>ff", "<cmd>Telescope find_files<cr>",  { noremap = true, silent = false })
+map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>",   { noremap = true, silent = false })
+map("n", "<leader>fb", "<cmd>Telescope buffers<cr>",     { noremap = true, silent = false })
+map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>",   { noremap = true, silent = false })
+
+-- fzf
+map("n", "<C-s>g", ":Rg<Space>",       { noremap = true, silent = false })
+map("n", "<C-s>f", ":GFiles<Space>",   { noremap = true, silent = false })
+map("n", "<C-s>h", ":History<CR>",     { noremap = true, silent = false })
+map("n", "<Leader>b", ":Buffers<CR>",  { noremap = true, silent = false })
+map("n", "<Leader>s", ":BLines<CR>",   { noremap = true, silent = false })
