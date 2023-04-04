4cf516daea7e8aed9d743d7c98ed01e23611c789
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-04-05 03:59:34 +0900

    Add lsp config

--- nvim/lua/plug.lua
+++ nvim/lua/plug.lua
@@ -17,6 +17,8 @@ Plug("flazz/vim-colorschemes")
 Plug("simeji/winresizer")
 Plug("nvim-lua/plenary.nvim")
 Plug("nvim-telescope/telescope.nvim", { tag ="0.1.1" })
+Plug("neovim/nvim-lspconfig")
+Plug("hrsh7th/nvim-cmp")
 
 vim.call("plug#end")
 
@@ -26,8 +28,8 @@ vim.g.NERDTreeShowHidden = 1
 -- indentline
 vim.g.indentLine_enabled = 1
 -- TODO: replace embeded func
-vim.cmd("autocmd Filetype json let g:indentLine_enabled = 0")
-vim.g.vim_json_syntax_conceal = 0
+-- vim.cmd("autocmd Filetype json let g:indentLine_enabled = 0")
+-- vim.g.vim_json_syntax_conceal = 0
 
 -- fzf
 local fzf_func = function(opts)
@@ -130,3 +132,56 @@ vim.g.gitgutter_sign_removed    = "-"
 
 -- winresizer
 vim.g.winresizer_start_key = "<C-W>w"
+
+-- nvim-lspconfig
+local lspconfig = require('lspconfig')
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  --Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  -- Mappings.
+  local opts = { noremap = true, silent = true }
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.format { async = true }<CR>", opts)
+end
+
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or 'single'
+  opts.max_width= opts.max_width or math.floor(vim.fn.winwidth(0) * 0.7)
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
+-- rust-analyzer
+lspconfig.rust_analyzer.setup {
+  on_attach = on_attach,
+  settings = {
+    ['rust-analyzer'] = {}
+  }
+}
+-- yamlls
+lspconfig.yamlls.setup{
+  settings = {
+    yaml = {
+      schemas = { kubernetes = "globPattern" },
+    }
+  }
+}
