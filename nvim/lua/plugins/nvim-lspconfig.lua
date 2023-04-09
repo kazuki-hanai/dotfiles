b39e1f276a698d9983f83e257c78ae1db6d04a95
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-04-09 23:08:26 +0900

    Fix nvim theme

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,6 @@
 -- Border settings
 vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
-vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=#777777]]
 
 local border = {
       {"🭽", "FloatBorder"},
