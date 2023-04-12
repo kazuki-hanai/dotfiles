30005759dbec7c8a768e8ea7b18d57ae58299eb5
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-04-13 04:22:46 +0900

    [nvim] Remove fzf, Fix telescope, Add colorscheme

--- nvim/lua/plug.lua
+++ nvim/lua/plug.lua
@@ -11,8 +11,6 @@ Plug("yggdroot/indentline")
 Plug("luochen1990/rainbow")
 Plug("tpope/vim-fugitive")
 Plug("airblade/vim-gitgutter")
-Plug("junegunn/fzf", { ["do"] = vim.fn["fzf#install()"] })
-Plug("junegunn/fzf.vim")
 Plug("jiangmiao/auto-pairs")
 Plug("simeji/winresizer")
 Plug("nvim-lua/plenary.nvim") --
@@ -33,6 +31,7 @@ Plug("phaazon/hop.nvim")
 -- ColorSchems
 Plug("flazz/vim-colorschemes")
 Plug("Mofiqul/vscode.nvim")
+Plug("folke/tokyonight.nvim", { ["branch"] = "main" })
 
 vim.call("plug#end")
 
@@ -100,19 +99,6 @@ vim.g.indentLine_enabled = 1
 -- vim.cmd("autocmd Filetype json let g:indentLine_enabled = 0")
 -- vim.g.vim_json_syntax_conceal = 0
 
--- fzf
-local fzf_func = function(opts)
-  vim.fn["fzf#vim#grep"](
-    "rg -u --column --line-number --hidden --ignore-case --no-heading --color=always --smart-case "..opts.args,
-    1,
-    opts.bang and
-      vim.fn["fzf#vim#with_preview"]({options = "--delimiter : --nth 4.."}, "up:60%") or
-      vim.fn["fzf#vim#with_preview"]({options = "--delimiter : --nth 4.."}, "right:50%", "?"),
-    opts.bang)
-end
-vim.api.nvim_create_user_command("Rg", fzf_func, { nargs = "*" })
-vim.g.rg_derive_root = "true"
-
 -- lightline
 local unpack = unpack or table.unpack
 table.slice = function(a, start ,_end)
@@ -140,14 +126,14 @@ require('lualine').setup {
     }
   },
   sections = {
-    lualine_a = {'diff', 'diagnostics'},
-    lualine_b = {
+    lualine_a = {},
+    lualine_b = {'diff', 'diagnostics'},
+    lualine_c = {
       {
         "filename",
         path = 1,
       }
     },
-    lualine_c = {},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
@@ -183,6 +169,17 @@ vim.g.gitgutter_sign_removed    = "-"
 
 -- winresizer
 vim.g.winresizer_start_key = "<C-W>w"
+
+-- telescope
+require("telescope").setup {
+  defaults = {
+  },
+  pickers = {
+      find_files = {
+          hidden = true,
+      },
+  },
+}
 
 -- nvim-lspconfig
 require("plugins/nvim-lspconfig")
@@ -238,3 +235,40 @@ vim.keymap.set('', 'T', function()
   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
 end, {remap=true})
 vim.keymap.set('', 'gh', ":HopWord<CR>", {})
+
+-- tokyonight
+require("tokyonight").setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = "day", -- The theme is used when the background is set to light
+  transparent = false, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "dark", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = false, -- dims inactive windows
+  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+  --- You can override specific color groups to use other groups or a hex color
+  --- function will be called with a ColorScheme table
+  ---@param colors ColorScheme
+  on_colors = function(colors) end,
+
+  --- You can override specific highlights to use other groups or a hex color
+  --- function will be called with a Highlights and ColorScheme table
+  ---@param highlights Highlights
+  ---@param colors ColorScheme
+  on_highlights = function(highlights, colors) end,
+})
