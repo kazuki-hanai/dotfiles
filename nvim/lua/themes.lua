d19f4ff19f26fca2bcbbc3d710855ee19463b6b7
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-04-13 04:24:21 +0900

    [nvim] Fix theme, remoe tokyonight

--- nvim/lua/themes.lua
+++ nvim/lua/themes.lua
@@ -1,5 +1,3 @@
 -- vim.cmd('colorscheme iceberg') 
 vim.cmd [[ set background=dark ]]
 vim.cmd [[ colorscheme vscode ]]
-vim.cmd [[ hi TabLineSel guifg=#ffffff guibg=#1e1e1e ]]
-vim.cmd [[ hi TabLine guifg=#aaaaaa ]]
