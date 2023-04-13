74ca7370fc500045f77ff99f2e1c21d6f7eaa080
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-04-13 16:28:16 +0900

    [nvim] Remove unnecessary border settings

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,30 +2,6 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      -- Border settings
-      vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
-      vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=#777777]]
-
-      local border = {
-            {"🭽", "FloatBorder"},
-            {"▔", "FloatBorder"},
-            {"🭾", "FloatBorder"},
-            {"▕", "FloatBorder"},
-            {"🭿", "FloatBorder"},
-            {"▁", "FloatBorder"},
-            {"🭼", "FloatBorder"},
-            {"▏", "FloatBorder"},
-      }
-
-      -- To instead override globally
-      local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-      function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-        opts = opts or {}
-        opts.border = opts.border or border
-        return orig_util_open_floating_preview(contents, syntax, opts, ...)
-      end
-
-      -- Global mappings.
       -- See `:help vim.diagnostic.*` for documentation on any of the below functions
       vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
       vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
