3b1bf0aeae985cfc6e4603a84a77eb8ce65425a5
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-04-13 16:48:48 +0900

    [nvim] Add gitlinker

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -137,6 +137,9 @@ return {
     end,
   },
   {
+    "nvim-lua/plenary.nvim",
+  },
+  {
     "tpope/vim-fugitive",
   },
   {
@@ -151,6 +154,18 @@ return {
   },
   {
     "ruifm/gitlinker.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("gitlinker").setup({
+        opts = {
+          action_callback = require("gitlinker.actions").copy_to_clipboard,
+        }
+      })
+      vim.api.nvim_set_keymap('n', 'gy', '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".copy_to_clipboard})<cr>', {silent = true})
+      vim.api.nvim_set_keymap('v', 'gy', '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".copy_to_clipboard})<cr>', {})
+    end,
   },
   {
     "jiangmiao/auto-pairs",
@@ -160,9 +175,6 @@ return {
     config = function()
       vim.g.winresizer_start_key = "<C-W>w"
     end,
-  },
-  {
-    "nvim-lua/plenary.nvim",
   },
   {
     "nvim-telescope/telescope.nvim",
