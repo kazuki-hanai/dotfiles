9b4127673fac1bc69e38bce24ef37b196bf9395b
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-04-13 17:01:59 +0900

    [nvim] Install nvim-treesitter

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -137,6 +137,21 @@ return {
     end,
   },
   {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = {
+          "c", "lua", "go", "cue", "json", "terraform",
+        },
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        }
+      }
+    end,
+  },
+  {
     "nvim-lua/plenary.nvim",
   },
   {
