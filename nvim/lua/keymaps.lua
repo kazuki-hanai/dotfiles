daf4b3536d87ac68705c9996c472d4bea4ca1de0
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-04-13 21:31:51 +0900

    [nvim] Add copy filepath keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -4,15 +4,24 @@ local map = vim.api.nvim_set_keymap
 map("i", "<S-Tab>", "<C-d>", { noremap = true, silent = false })
 
 -- Tab mappings
-map("n", "<C-t>t", ":tabnew<CR>",        { noremap = true, silent = false })
-map("n", "<C-l>", ":tabnext<CR>",       { noremap = true, silent = false })
-map("n", "<C-h>", ":tabprev<CR>",       { noremap = true, silent = false })
-map("n", "<C-t>l", ":+tabmove<CR>",      { noremap = true, silent = false })
-map("n", "<C-t>h", ":-tabmove<CR>",      { noremap = true, silent = false })
-map("i", "<C-t><C-t>", "<Esc>:tabnew<CR>",   { noremap = true, silent = false })
-map("i", "<C-l>", "<Esc>:tabnext<CR>",  { noremap = true, silent = false })
-map("i", "<C-h>", "<Esc>:tabprev<CR>",  { noremap = true, silent = false })
+map("n", "<C-t>t", ":tabnew<CR>",           { noremap = true, silent = false })
+map("n", "<C-l>", ":tabnext<CR>",           { noremap = true, silent = false })
+map("n", "<C-h>", ":tabprev<CR>",           { noremap = true, silent = false })
+map("n", "<C-t>l", ":+tabmove<CR>",         { noremap = true, silent = false })
+map("n", "<C-t>h", ":-tabmove<CR>",         { noremap = true, silent = false })
+map("i", "<C-t><C-t>", "<Esc>:tabnew<CR>",  { noremap = true, silent = false })
+map("i", "<C-l>", "<Esc>:tabnext<CR>",      { noremap = true, silent = false })
+map("i", "<C-h>", "<Esc>:tabprev<CR>",      { noremap = true, silent = false })
+
+-- Copy filepath to clipboard
+map("n", "<space>c", "",{
+  callback = function()
+    vim.fn.setreg("+", vim.fn.expand('%'))
+  end,
+  noremap = true,
+  silent = false
+})
 
 -- nvim-tree
 map("n", "<C-b>f", ":NvimTreeFindFile<cr>",   { noremap = true, silent = false })
-map("n", "<C-b>n", ":NvimTreeToggle<cr>", { noremap = true, silent = false })
+map("n", "<C-b>n", ":NvimTreeToggle<cr>",     { noremap = true, silent = false })
