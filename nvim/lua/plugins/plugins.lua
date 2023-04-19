bcc1e6852c30b35d64a0423cce68adaeea22adbf
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-04-19 14:53:56 +0900

    [nvim] Fix inactive tab color

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -213,6 +213,8 @@ return {
   {
     "simeji/winresizer",
     config = function()
+      vim.g.winresizer_start_key = '<C-w>w'
+      vim.g.winresizer_gui_start_key = '<C-w>w'
       vim.api.nvim_set_keymap('n', '<C-w>w', '<cmd>WinResizerStartResize<cr>', {silent = true})
     end,
   },
