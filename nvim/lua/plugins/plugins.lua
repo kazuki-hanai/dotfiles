236d174398f437146b06989a731fd99d784b2dec
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-05-02 01:55:08 +0900

    [nvim] Add charscount in lualine

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -55,6 +55,19 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     config = function()
+      local function charscount()
+        local mode = vim.fn.mode(true)
+        local line_start = vim.fn.line('v')
+        local line_end = vim.fn.line('.')
+        local line_num = tostring(math.abs(line_start - line_end) + 1)
+        local chars_num = tostring(vim.fn.wordcount()['visual_chars'])
+        if mode:match('[vV]') then
+          return line_num .. ':' .. chars_num
+        else
+          return ''
+        end
+      end
+
       require('lualine').setup {
         options = {
           icons_enabled = true,
@@ -83,9 +96,9 @@ return {
               path = 1,
             },
           },
-          lualine_x = { 'encoding', 'fileformat', 'filetype' },
-          lualine_y = { 'progress' },
-          lualine_z = { 'location' }
+          lualine_x = { charscount, 'encoding', 'fileformat', 'filetype', 'location', 'progress' },
+          lualine_y = { },
+          lualine_z = { }
         },
         inactive_sections = {
           lualine_a = {},
