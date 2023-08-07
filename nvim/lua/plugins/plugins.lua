14aff17a92e5b639b657bce23220178005fa37a0
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-08-07 18:17:10 +0900

    Add copilot.vim

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,8 +1,8 @@
 return {
   {
     "windwp/nvim-autopairs",
-    config = function ()
-      require("nvim-autopairs").setup{}
+    config = function()
+      require("nvim-autopairs").setup {}
     end
   },
   {
@@ -39,4 +39,69 @@ return {
   {
     "Mofiqul/vscode.nvim",
   },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    version = "1.*",
+    build = "make install_jsregexp"
+  },
+  -- {
+  --   "glepnir/lspsaga.nvim",
+  --   event = "LspAttach",
+  --   version = "v0.2.9",
+  --   config = function()
+  --     require("lspsaga").setup({})
+  --   end,
+  --   dependencies = { { "nvim-tree/nvim-web-devicons" } },
+  --   keys = {
+  --     -- { "gh",         "<cmd>Lspsaga lsp_finder<CR>" },
+  --     { "<leader>ca", { "<cmd>Lspsaga code_action<CR>", mode = { "n", "v" } } },
+  --     { "gr",         "<cmd>Lspsaga rename<CR>" },
+  --     { "gr",         "<cmd>Lspsaga rename ++project<CR>" },
+  --     { "gp",         "<cmd>Lspsaga peek_definition<CR>" },
+  --     { "gd",         "<cmd>Lspsaga goto_definition<CR>" },
+  --     { "gt",         "<cmd>Lspsaga peek_type_definition<CR>" },
+  --     { "gt",         "<cmd>Lspsaga goto_type_definition<CR>" },
+  --     { "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>" },
+  --     { "<leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>" },
+  --     { "<leader>sw", "<cmd>Lspsaga show_workspace_diagnostics<CR>" },
+  --     { "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>" },
+  --     { "[e",         "<cmd>Lspsaga diagnostic_jump_prev<CR>" },
+  --     { "]e",         "<cmd>Lspsaga diagnostic_jump_next<CR>" },
+  --     { "[E", function()
+  --       require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+  --     end },
+  --     { "]E", function()
+  --       require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
+  --     end },
+  --     { "<leader>o", "<cmd>Lspsaga outline<CR>" },
+  --     { "K",         "<cmd>Lspsaga hover_doc<CR>" },
+  --     { "K",         "<cmd>Lspsaga hover_doc ++keep<CR>" },
+  --     -- { "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>" },
+  --     -- { "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>" },
+  --     -- { "<A-d>",      { "<cmd>Lspsaga term_toggle<CR>", mode = { "n", "t" } } },
+  --   },
+  -- },
+  {
+    "github/copilot.vim",
+    config = function()
+      vim.g.copilot_filetypes = {
+        bash = true,
+        javascript = true,
+        typescript = true,
+        yaml = true,
+        json = true,
+        go = true,
+        rust = true,
+        python = true,
+        lua = true,
+        markdown = true,
+        html = true,
+        css = true,
+      }
+    end,
+  }
 }
