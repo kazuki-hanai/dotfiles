59c3c124179ff826d6607563900788095267ff18
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-10-17 18:57:46 +0900

    fix

---
+++ zsh/tools/krew.sh
@@ -0,0 +1,14 @@
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+# Install krew
+if ! kubectl krew 2>&1 >/dev/null; then
+  (
+    set -x; cd "$(mktemp -d)" &&
+    OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+    ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+    KREW="krew-${OS}_${ARCH}" &&
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+    tar zxvf "${KREW}.tar.gz" &&
+    ./"${KREW}" install krew
+  )
+fi
