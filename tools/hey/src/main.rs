2f3c34268482acddb2ee610d82ebf3811928cd8a
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2023-10-17 20:52:00 +0900

    Add hey

---
+++ tools/hey/src/main.rs
@@ -0,0 +1,59 @@
+use chat_gpt_lib_rs::{ChatGPTClient, ChatInput, Message, Model, Role};
+use colored::*;
+use dotenv::from_path;
+use spinners::{Spinner, Spinners};
+
+use std::env;
+
+const OPENAI_BASE_URL: &str = "https://api.openai.com";
+
+fn get_home_dir() -> String {
+    match env::var("HOME") {
+        Ok(val) => val,
+        Err(e) => panic!("Error: {}", e),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    match from_path(format!("{}/.config/hey/.env", get_home_dir())) {
+        Ok(()) => {}
+        Err(e) => panic!("Error: {}", e),
+    };
+
+    let openai_api_key = match env::var("OPENAI_API_KEY") {
+        Ok(val) => val,
+        Err(e) => panic!("Error: {}", e),
+    };
+
+    let question: String = env::args().collect::<Vec<String>>()[1..].join(" ");
+
+    let client = ChatGPTClient::new(&openai_api_key, OPENAI_BASE_URL);
+    let chat_input = ChatInput {
+        model: Model::Gpt3_5Turbo,
+        messages: vec![
+            Message {
+                role: Role::System,
+                content: "You are a helpful assistant.".to_string(),
+            },
+            Message {
+                role: Role::User,
+                content: question,
+            },
+        ],
+        ..Default::default()
+    };
+
+    let mut sp = Spinner::new(
+        Spinners::Dots9,
+        "Waiting for response from OpenAI"
+            .bright_black()
+            .to_string(),
+    );
+    let response = client.chat(chat_input).await.unwrap();
+    sp.stop_and_persist("", "".to_string());
+
+    for choice in &response.choices {
+        println!("{}", choice.message.content);
+    }
+}
