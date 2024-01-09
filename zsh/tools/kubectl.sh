169c0f63f3e52f424d0b727044b089f3015ffe6e
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2024-01-10 00:30:01 +0900

    Add kubectl installing

--- zsh/tools/kubectl.sh
+++ zsh/tools/kubectl.sh
@@ -1,1 +1,10 @@
+if ! type kubectl > /dev/null 2>&1; then
+  if [ "$(uname)" = 'Darwin' ] && ! existsCmd brew; then
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl.sha256"
+  elif [ -e /etc/lsb-release ]; then
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  fi
+  chmod +x kubectl
+  mv ./kubectl ~/.bin/kubectl
+fi
 alias k=kubectl
