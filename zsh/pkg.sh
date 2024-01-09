6fab1e317737a574c66e74d7862fd9120fa3db4c
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2024-01-10 00:00:05 +0900

    Fix installing

--- zsh/pkg.sh
+++ zsh/pkg.sh
@@ -19,8 +19,17 @@ installUbuntu tk-dev
 installUbuntu llvm
 installUbuntu clang
 installUbuntu ca-certificates
-installUbuntu ripgrep rg
 installUbuntu git-delta
+
+# gh command
+if [ -e /etc/lsb-release ] && ! type gh > /dev/null 2>&1; then
+  type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+  && sudo apt update \
+  && sudo apt install gh -y
+fi
 
 installMac gomi b4b4r07/tap/gomi
 installMac gh
