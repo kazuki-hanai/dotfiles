f9d4ae9224fb27e2630c936e0abf501fc353689b
Kazuki Hanai <kazuki.hanai@wan-nyan-wan.net>
2024-02-17 23:36:06 +0900

    refactor iroiro

--- zsh/tools/fzf.sh
+++ zsh/tools/fzf.sh
@@ -1,3 +1,18 @@
+## fzf
+if [ ! -d ~/.fzf ]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    # yes yes no
+    echo 'y\ny\nn' | ~/.fzf/install
+fi
+
+# fzf
+export FZF_DEFAULT_COMMAND="rg -u --files --hidden --follow --glob '!.git' 2> /dev/null"
+export FZF_CTRL_T_COMMAND='rg -u --files --hidden --follow --glob "!.git/*" 2> /dev/null'
+export FZF_CTRL_T_OPTS='--preview "bat  --color=always --style=header,grid --line-range :100 {}"'
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+
+########### fzf functions ###########
 
 # fd - cd to selected directory
 fd() {
