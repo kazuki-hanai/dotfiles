ed19f77743c810a313afc1c1a4ccea45da10c536
hnkz <hnkz.64@gmail.com>
2025-01-24 14:52:07 +0900

    Refactor auto-commit script, rename suggest script, and update config templates

--- zsh/brew.sh
+++ zsh/brew.sh
@@ -5,7 +5,7 @@ elif isUbuntu; then
   brew_path="/home/linuxbrew/.linuxbrew/bin/brew"
 fi
 
-if $brew_path 1>/dev/null 2&>1; then
+if $brew_path >/dev/null 2>&1; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
